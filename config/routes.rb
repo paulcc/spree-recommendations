81df030f60e20b93d80958b9f0a3e806031a5882
paulcc <paulcc.two@gmail.com>
2009-06-19 15:04:15 +0100

    fix routes and translations

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 # Put your extension routes here.
 
-# map.namespace :admin do |admin|
-#   admin.resources :whatever
-# end  
-
 map.namespace :admin do |admin|
    admin.resources :products do |product|
-     product.resources :accessories, :member => {:select => :post, :remove => :post}, :collection => {:available => :post, :selected => :get}
+     product.resources :recommendations, :member => {:select => :post, :remove => :post}, :collection => {:available => :post, :selected => :get}
    end
 end  
